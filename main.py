74d4ae411050b1d0ba7b55036d11c3973b65b320
leblancfg <leblancfg@gmail.com>
2020-03-26 11:51:12 -0400

    versioned api

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import io
+import logging
 import os
 import tempfile
 
@@ -5,8 +7,11 @@ import cv2
 import numpy as np
 from PIL import Image
 from autocrop.autocrop import Cropper
-from autocrop.constants import CV2_FILETYPES, PILLOW_FILETYPES
+
 from fastapi import FastAPI, File, UploadFile
+from fastapi_versioning import VersionedFastAPI, version
+from pydantic import AnyHttpUrl
+import requests
 from starlette.responses import FileResponse
 from starlette.middleware.cors import CORSMiddleware
 
@@ -18,14 +23,14 @@ app.add_middleware(
 
 def open_file(file, extension):
     """Given a filename, returns a numpy array"""
-
-    if extension in CV2_FILETYPES:
-        # Try with cv2
-        return cv2.imread(file.file)
-    if extension in PILLOW_FILETYPES:
+    try:
         # Try with PIL
-        with Image.open(file.file) as img_orig:
+        with Image.open(file) as img_orig:
             return np.asarray(img_orig)
+    except Exception:
+        # Try with cv2
+        # TODO: this might not be working
+        return cv2.imread(file)
     return None
 
 
@@ -43,7 +48,8 @@ def upload_img_file(img, ext: str):
 
 
 @app.post("/crop")
-async def image_endpoint(
+@version(1)
+async def crop(
     width: int = 500,
     height: int = 500,
     face_percent: int = 50,
@@ -56,7 +62,7 @@ async def image_endpoint(
     c = Cropper(width=width, height=height, face_percent=face_percent)
 
     # Crop
-    img = open_file(file, file_extension)
+    img = open_file(file.file, file_extension)
     img_array = c.crop(img)
     if img_array is None:
         return None
@@ -67,6 +73,45 @@ async def image_endpoint(
     return upload_img_file(img=byte_im, ext=file_extension)
 
 
+@app.post("/crop_uri")
+@version(1)
+async def crop_uri(
+    uri: AnyHttpUrl, width: int = 500, height: int = 500, face_percent: int = 50,
+):
+    """Returns a cropped form of the document image."""
+    _, file_extension = os.path.splitext(uri)
+
+    # Set up Cropper instance and crop
+    c = Cropper(width=width, height=height, face_percent=face_percent)
+
+    # Crop
+    r = requests.get(uri, stream=True)
+    if r.status_code != 200:
+        # TODO
+        return None
+    file = io.BytesIO(r.content)
+
+    img = open_file(file, file_extension)
+    img_array = c.crop(img)
+    if img_array is None:
+        # TODO
+        return None
+
+    # Convert to bytes
+    is_success, img_buffer = cv2.imencode(file_extension, img_array)
+    byte_im = img_buffer.tobytes()
+    return upload_img_file(img=byte_im, ext=file_extension)
+
+
 @app.get("/")
+@version(1)
 def home():
-    return {"autocrop API":{'version': '1', 'source': 'https://github.com/leblancfg/autocrop'}}
+    return {
+        "autocrop API": {
+            "version": "1",
+            "source": "https://github.com/leblancfg/autocrop",
+        }
+    }
+
+
+app = VersionedFastAPI(app, version_format="{major}", prefix_format="/v{major}",)
