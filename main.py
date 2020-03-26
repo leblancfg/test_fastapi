9eb0eab2cc25ee815408e03b09ed2aad06d7c845
leblancfg <leblancfg@gmail.com>
2020-03-26 09:52:53 -0400

    removed extraneous folder

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 from PIL import Image
 from autocrop.autocrop import Cropper
+from autocrop.constants import CV2_FILETYPES, PILLOW_FILETYPES
 from fastapi import FastAPI, File, UploadFile
 from starlette.responses import FileResponse
 from starlette.middleware.cors import CORSMiddleware
@@ -15,10 +16,23 @@ app.add_middleware(
 )
 
 
-def open_file(file):
-    """Given a file object, returns a numpy array"""
-    with Image.open(file.file) as img_orig:
-        return np.asarray(img_orig)
+def open_file(file, extension):
+    """Given a filename, returns a numpy array"""
+
+    if extension in CV2_FILETYPES:
+        # Try with cv2
+        return cv2.imread(file.file)
+    if extension in PILLOW_FILETYPES:
+        # Try with PIL
+        with Image.open(file.file) as img_orig:
+            return np.asarray(img_orig)
+    return None
+
+
+# def open_file(file):
+#     """Given a file object, returns a numpy array"""
+#     with Image.open(file.file) as img_orig:
+#         return np.asarray(img_orig)
 
 
 def upload_img_file(img, ext: str):
@@ -42,10 +56,17 @@ async def image_endpoint(
     c = Cropper(width=width, height=height, face_percent=face_percent)
 
     # Crop
-    img = open_file(file)
+    img = open_file(file, file_extension)
     img_array = c.crop(img)
+    if img_array is None:
+        return None
 
     # Convert to bytes
     is_success, img_buffer = cv2.imencode(file_extension, img_array)
     byte_im = img_buffer.tobytes()
     return upload_img_file(img=byte_im, ext=file_extension)
+
+
+@app.get("/")
+def home():
+    return {"autocrop API":{'version': '1', 'source': 'https://github.com/leblancfg/autocrop'}}
